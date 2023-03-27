3745fd98d05d99b640b376d4057e82a92263e42c
michael <raschmichael10@gmail.com>
2023-03-27 18:08:54 -0500

    Initial Commit

---
+++ StockMarketUI/StockMarketUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockMarketUIApp.swift
+//  StockMarketUI
+//
+//  Created by Michael Rasch on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct StockMarketUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StockMarketUIUITests/StockMarketUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StockMarketUIUITestsLaunchTests.swift
+//  StockMarketUIUITests
+//
+//  Created by Michael Rasch on 3/27/23.
+//
+
+import XCTest
+
+final class StockMarketUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
